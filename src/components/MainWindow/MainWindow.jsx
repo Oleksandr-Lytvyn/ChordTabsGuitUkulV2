00365ba5d4432889a6983819ebdb3935edeba8a3
Oleksandr Lytvyn <tvinlee7@gmail.com>
2023-10-01 19:47:44 +0300

    delete unused

--- src/components/MainWindow/MainWindow.jsx
+++ src/components/MainWindow/MainWindow.jsx
@@ -1,11 +1,4 @@
-// import { useChord } from 'context/chordContext';
-
 export const MainWindow = ({ children }) => {
-  //   const chord = useChord();
-  //   console.log(chord);
-  //   const onClick = () => {
-  //     chord.keySet();
-  //   };
   return (
     <section className="main-section">
       <div className="container">{children}</div>
